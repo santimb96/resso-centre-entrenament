634f87c6918f325909808b7242cca8d3820c34b4
santimb96 <santiagomartinezbota@gmail.com>
2025-05-04 00:45:32 +0200

    [feat]: improved slider component (UX approach)

--- app/components/slider.jsx
+++ app/components/slider.jsx
@@ -2,19 +2,79 @@
 
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
+import { useState } from 'react'
 
 export default function Slider ({ children }) {
-  const [sliderRef] = useKeenSlider({
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider({
     loop: true,
+    initial: 0,
     slides: {
       perView: 1,
       spacing: 5,
     },
+    slideChanged (slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created () {
+      setLoaded(true)
+    },
   })
 
   return (
-    <div ref={sliderRef} className="keen-slider">
-      {children}
+    <div className='relative w-full flex-col justify-center items-center gap-2.5'>
+      <div ref={sliderRef} className="keen-slider">
+        {children}
+      </div>
+      {loaded && instanceRef.current && (
+        <>
+          <div className='flex justify-center gap-2.5 mt-14'>
+            <Arrow
+              left
+              onClick={(e) => e.stopPropagation() || instanceRef.current?.prev()} />
+
+            <Arrow
+              onClick={(e) => e.stopPropagation() || instanceRef.current?.next()} />
+          </div>
+          <div className="dots">
+            {[
+              ...Array(instanceRef.current.track.details.slides.length).keys(),
+            ].map((idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    instanceRef.current?.moveToIdx(idx)
+                  } }
+                  className={'dot' + (currentSlide === idx ? ' active' : '')}
+                ></button>
+              )
+            })}
+          </div></>
+      )
+      }
     </div>
+  )
+}
+
+function Arrow(props) {
+  const disabled = props.disabled ? ' arrow--disabled' : ''
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow textShadow ${
+        props.left ? 'arrow--left' : 'arrow--right'
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
   )
 }
