e5915aa55a93f02db5077913a18dfee5a98da1c7
santimb96 <santiagomartinezbota@gmail.com>
2025-05-10 00:54:09 +0200

    [feat]: removed view transition package

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,20 @@
 import Footer from '@/components/footer'
 import Header from '@/components/headerComponents/header'
-import { ViewTransitions } from 'next-view-transitions'
 import ScrollToTop from './components/scrollToTop'
 import './globals.css'
 import { notoSans } from './lib/fonts'
 
 export default function RootLayout({ children }) {
   return (
-    <ViewTransitions>
-      <html lang="es">
-        <body className={`${notoSans.className} flex flex-col items-center justify-start relative`}>
-          <Header />
-          <section className='w-full flex flex-col items-start gap-5 md:px-0'>
-            {children}
-          </section>
-          <ScrollToTop />
-          <Footer />
-        </body>
-      </html>
-    </ViewTransitions>
+    <html lang="es">
+      <body className={`${notoSans.className} flex flex-col items-center justify-start relative`}>
+        <Header />
+        <section className='w-full flex flex-col items-start gap-5 md:px-0'>
+          {children}
+        </section>
+        <ScrollToTop />
+        <Footer />
+      </body>
+    </html>
   )
 }
