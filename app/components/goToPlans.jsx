16bdf44927442b90b05a602094050a12b4d44434
santimb96 <santiagomartinezbota@gmail.com>
2025-05-10 01:22:45 +0200

    [feat]: added "ver planes" fixed button to the main page

---
+++ app/components/goToPlans.jsx
@@ -0,0 +1,7 @@
+export default function GoToPlans() {
+  return (
+    <a href='#planes' className='z-50 fixed bottom-5 self-center bg-primary border border-secondary rounded-full px-5 py-2.5 text-secondary hover:scale-110 duration-150 ease-in-out hover:bg-accent hover:text-primary hover:border-primary'>
+        Ver planes
+    </a>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -4,15 +4,17 @@ import Presentation from '@/components/presentation'
 import Sponsors from '@/components/sponsors'
 import WhoIAm from '@/components/whoIAm'
 import WorkshopsSection from '@/components/workshopComponents/workshopsSection'
+import GoToPlans from './components/goToPlans'
 export default function Home() {
   return (
-    <section className='flex flex-col w-full'>
+    <section className='relative flex flex-col w-full'>
       <Presentation />
       <WhoIAm />
       <Plans />
       <WorkshopsSection />
       <Sponsors />
       <Contact />
+      <GoToPlans />
     </section>
   )
 }
