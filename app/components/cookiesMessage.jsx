43578a4fcc8ffc20f491bd5ddcc408db0cbaeae3
santimb96 <santiagomartinezbota@gmail.com>
2025-05-19 16:31:35 +0200

    [feat]: added cookies approach to mobile viewport

--- app/components/cookiesMessage.jsx
+++ app/components/cookiesMessage.jsx
@@ -28,14 +28,17 @@ export default function CookiesMessage() {
 
   return (
     !cookiesAllowed && (
-      <div className='z-50 border border-secondary fixed bottom-5 bg-primary shadow-custom rounded-md px-5 py-2.5 flex justify-start items-center gap-2.5'>
-        <span className='min-w-6'><Cookie /></span>
-        <p>Este sitio usa cookies. Para obtener más información, consulta nuestra <Link href="/cookies-policy" className='text-sky-500 hover:text-sky-600 hover:underline duration-150 ease-in-out' target='_blank'>política de cookies</Link></p>
-        <div className='flex justify-center items-center'>
-          <button onClick={() => saveCookiesPreference(true)} className='min-w-8 rounded-full duration-150 ease-in-out text-green-400 hover:text-green-600'><CheckCircle size={32} /></button>
-          <button onClick={() => saveCookiesPreference(false)} className='min-w-8 rounded-full duration-150 ease-in-out text-red-400 hover:text-red-600'><CloseCircle size={32} /></button>
+      <>
+        <div className='z-50 border border-secondary fixed bottom-5 bg-primary shadow-custom rounded-md px-5 py-2.5 hidden md:flex md:w-[80%] lg:w-[70%] xl:w-[60%] justify-start items-center gap-2.5'>
+          <span className='min-w-6'><Cookie /></span>
+          <p>Este sitio usa cookies. Para obtener más información, consulta nuestra <Link href="/cookies-policy" className='text-sky-500 hover:text-sky-600 hover:underline duration-150 ease-in-out' target='_blank'>política de cookies</Link></p>
+          <div className='flex justify-center items-center'>
+            <button onClick={() => saveCookiesPreference(true)} className='min-w-8 rounded-full duration-150 ease-in-out text-green-400 hover:text-green-600'><CheckCircle size={32} /></button>
+            <button onClick={() => saveCookiesPreference(false)} className='min-w-8 rounded-full duration-150 ease-in-out text-red-400 hover:text-red-600'><CloseCircle size={32} /></button>
+          </div>
         </div>
-      </div>
+        <button onClick={() => saveCookiesPreference(true)} className='z-50 fixed bottom-20 right-5 bg-primary shadow-custom rounded-full block md:hidden border border-secondary p-1'><Cookie size={36} /></button>
+      </>
     )
   )
 }
