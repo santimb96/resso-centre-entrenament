37078a49a67bcd5476732ebf95432e55f687ff40
santimb96 <santiagomartinezbota@gmail.com>
2025-06-12 16:11:38 +0200

    [fix]: replaced texts // fixed UI issues

--- app/components/workshopComponents/workshopsSection.jsx
+++ app/components/workshopComponents/workshopsSection.jsx
@@ -8,7 +8,7 @@ export default function WorkshopsSection () {
     <section id='workshops' className=' flex flex-col justify-center items-center gap-5 scroll-m-24 bg-accent py-5 px-2.5'>
       <div className={WIDTH_LAYOUT}>
         <TitleSection title='Workshops' />
-        <TextComponent text='Aquí se detallan los próximos talleres que tendrán lugar en <i class="underline">#BeRessò</i>' textColor='primary' />
+        <TextComponent text='Aquí podrás encontrar los próximos talleres que tendrán lugar en el centro' textColor='primary' />
         <div className='mt-10'>
           <Workshops />
         </div>
