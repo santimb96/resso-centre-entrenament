2d445f95fe758baa20304518cb013cba1b9bf66a
santimb96 <santiagomartinezbota@gmail.com>
2025-06-13 18:13:50 +0200

    [fix]: improved accessibility link texts

--- app/components/workshopComponents/workshop.jsx
+++ app/components/workshopComponents/workshop.jsx
@@ -51,7 +51,7 @@ export default function Workshop({
           href={`/workshops/${id}-${title.toLowerCase()}`}
           className='font-semibold flex items-center gap-2.5 border border-primary hover:bg-gray-900/70 px-5 py-2.5 rounded-md duration-150 ease-in-out'
         >
-          Más información
+          Ver workshop
           <span className='min-w-6 w-6'>
             <ExternalLink size={20} />
           </span>
