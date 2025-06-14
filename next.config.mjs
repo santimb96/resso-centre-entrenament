73648ae5f836fe3e70fbeb8ff1e3b55295816bbf
santimb96 <santiagomartinezbota@gmail.com>
2025-06-14 16:42:12 +0200

    [fix]: added redirect approach

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.ressocentredentrenament.com', // with www
+          },
+        ],
+        destination: 'https://ressocentredentrenament.com/:path*', // without www
+        permanent: true, // 301 redirect
+      },
+    ]
+  },
 }
+
 
 export default nextConfig
