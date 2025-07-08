228d940d8963f30696bd569efe895405e08cebb0
santimb96 <santiagomartinezbota@gmail.com>
2025-07-08 15:18:32 +0200

    [feat]: added favicon approach

--- app/layout.jsx
+++ app/layout.jsx
@@ -92,12 +92,11 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <head>
         <meta name="apple-mobile-web-app-title" content="Ressò - Centre d'entrenament" />
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
         <meta name="apple-mobile-web-app-title" content="Ressò - Centre d'entrenament" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLD) }} />
       </head>
