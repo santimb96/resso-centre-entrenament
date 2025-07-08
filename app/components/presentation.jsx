d8ba1b7ad63224b422d82cef0a76d8cda512dd0f
santimb96 <santiagomartinezbota@gmail.com>
2025-07-09 00:54:18 +0200

    [fix]: added fetchpriority high

--- app/components/presentation.jsx
+++ app/components/presentation.jsx
@@ -9,6 +9,7 @@ export default function Presentation () {
         src='/img/headerImg.webp'
         alt='Imagen de cabecera de la zona de entrenamiento'
         fill
+        fetchPriority='high'
         priority
         className='object-cover object-center grayscale-[100%] contrast-[150%] brightness-[115%] z-[-1]'
       />
