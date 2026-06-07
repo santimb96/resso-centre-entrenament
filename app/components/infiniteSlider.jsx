234d66627f3854e17b17d6f1c40ec47f1e3638e9
Santi Martínez <santiagomartinezbota@gmail.com>
2026-06-07 20:06:19 +0200

    feat: replace @devnomic/marquee with custom marquee implementation; enhance styling and performance

--- app/components/infiniteSlider.jsx
+++ app/components/infiniteSlider.jsx
@@ -1,14 +1,17 @@
-import { Marquee } from '@devnomic/marquee'
-// if you copy ala shadcn, no need import css.
-import '@devnomic/marquee/dist/index.css'
 import Image from 'next/image'
 
-export default function InfiniteSlider ({ sponsors }) {
+export default function InfiniteSlider({ sponsors }) {
   return (
-    <Marquee fade={true} pauseOnHover={true}>
-      {sponsors.map((sponsor, index) => (
-        <Image key={index} src={sponsor.image} alt={sponsor.name} width={sponsor.width} height={sponsor.height} />
-      ))}
-    </Marquee>
+    <div className='marquee-container'>
+      <div className='marquee-track'>
+        {/* Duplicamos para loop infinito sin saltos */}
+        {sponsors.map((sponsor, index) => (
+          <Image key={index} src={sponsor.image} alt={sponsor.name} width={sponsor.width} height={sponsor.height} />
+        ))}
+        {sponsors.map((sponsor, index) => (
+          <Image key={`dup-${index}`} src={sponsor.image} alt={sponsor.name} width={sponsor.width} height={sponsor.height} />
+        ))}
+      </div>
+    </div>
   )
 }
